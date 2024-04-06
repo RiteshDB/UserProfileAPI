53eddde31ab9cd4bd95f66cf3150bd15c3e4a428
RiteshDB <riteshdb123@gmail.com>
2024-04-06 22:43:39 +0530

    application set up completed.

---
+++ src/main/java/com/ridham/userProfile/UserProfileApplication.java
@@ -0,0 +1,15 @@
+package com.ridham.userProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UserProfileApplication.class, args);
+		System.out.println("Application is running");
+	}
+
+}
